1c9de65b432d4047fde63d49de8a0bb2802e34e4
SergiuChilat <chilatsergiu@gmail.com>
2019-09-28 13:40:15 +0300

    Pattern Facade: after

--- Facade.php
+++ Facade.php
@@ -20,21 +20,31 @@ class SuperFormula {
 
 class Facade
 {
-
-}
-
-$f = new SuperFormula();
-
-$x = 2;
-$y = 3;
-
-if($x > 0) {
-    $x = $f->calculateStep1($x);
-    if($x < 1000) {
-        $x = $f->calculateStep2($x);
-        if($x > 1000 && $y > 0) {
-            $x = $f->calculateStep3($x, $y);
+    public function calculateSuperFormula($x, $y, $f)
+    {
+        if($x > 0) {
+            $x = $f->calculateStep1($x);
+            if($x < 1000) {
+                $x = $f->calculateStep2($x);
+                if($x > 1000 && $y > 0) {
+                    $x = $f->calculateStep3($x, $y);
+                }
+            }
         }
+        return $x;
     }
 }
 
+
+
+
+
+
+
+
+$f = new SuperFormula();
+$x = 2;
+$y = 3;
+
+$calculator = new Facade();
+echo $calculator->calculateSuperFormula($x, $y, $f);
